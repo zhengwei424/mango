3c8f75b3a3bedd940a376bfc4bf835b0067a0532
zhengwei424 <358025194@qq.com>
2024-10-29 12:48:42 +0800

    update

--- mango-monitor/src/main/java/com/zhengwei/mango/monitor/MangoMonitorApplication.java
+++ mango-monitor/src/main/java/com/zhengwei/mango/monitor/MangoMonitorApplication.java
@@ -4,13 +4,9 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * 启动器
- */
 @EnableAdminServer
 @SpringBootApplication
-class MangoMonitorApplication {
-
+public class MangoMonitorApplication {
     public static void main(String[] args) {
         SpringApplication.run(MangoMonitorApplication.class, args);
     }

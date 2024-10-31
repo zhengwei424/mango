fde37e989cd4824f4be2bb6a508e106900fa9139
zhengwei424 <358025194@qq.com>
2024-10-31 17:22:00 +0800

    update

--- mango-consumer/src/main/java/com/zhengwei/mango/consumer/MangoConsumerApplication.java
+++ mango-consumer/src/main/java/com/zhengwei/mango/consumer/MangoConsumerApplication.java
@@ -1,13 +1,32 @@
 package com.zhengwei.mango.consumer;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@RibbonClient(name = "server", configuration = MangoConsumerApplication.class)
 public class MangoConsumerApplication {
     public static void main(String[] args) {
         SpringApplication.run(MangoConsumerApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public IRule randomRule() {
+        return new RandomRule();
     }
 }

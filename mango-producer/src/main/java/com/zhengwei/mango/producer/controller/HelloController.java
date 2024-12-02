a65ecd5e4d9a1029017a8f9db513bfb758df1a11
zhengwei424 <358025194@qq.com>
2024-12-02 08:45:24 +0800

    update

--- mango-producer/src/main/java/com/zhengwei/mango/producer/controller/HelloController.java
+++ mango-producer/src/main/java/com/zhengwei/mango/producer/controller/HelloController.java
@@ -1,12 +1,17 @@
 package com.zhengwei.mango.producer.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    @Value("${hello}")
+    private String hello;
+
     @RequestMapping("/hello")
     public String hello() {
-        return "Hello Mango!";
+        return hello;
     }
 }
